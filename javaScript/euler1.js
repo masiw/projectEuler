fe585707ea5e4c499744f90e7a0405bad14008a5
masi <masi.woermann@gmail.com>
2016-09-01 11:37:57 +0100

    Euler 1 - Refined solution... - changed to ECMA 6 - Created module.

--- javaScript/euler1.js
+++ javaScript/euler1.js
@@ -1,8 +1,2 @@
-function euler1() {
-  function triangularNumber(n) {
-    return n * (n + 1) / 2
-  }
-  return 3 * triangularNumber(999 / 3) +
-    5 * triangularNumber(995 / 5) -
-    15 * triangularNumber(990 / 15)
-}
+// tn -> nth triangular number
+module.exports = () => (tn => 3 * tn(999/3) + 5 * tn(995/5) - 15 * tn(990/15))(n => n * (n + 1) / 2)
